6b4b6e10c4c7854695aaf4ba49842f5fdd388f12
Alan Levine <cogdogblog@gmail.com>
2016-03-07 09:08:55 -0700

    Title Fix
    Wordpress changed the functionality of wp_title()- this fixes a
    situation where you get a blank <title></title> in the header, see
    https://make.wordpress.org/core/2014/10/29/title-tags-in-4-1/

--- wp-content/themes/tru-writer/header.php
+++ wp-content/themes/tru-writer/header.php
@@ -6,9 +6,20 @@
 	<head profile="http://gmpg.org/xfn/11">
 		
 		<meta http-equiv="Content-Type" content="<?php bloginfo('html_type'); ?>; charset=<?php bloginfo('charset'); ?>" />
-		<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" >
-																		
-		<title><?php wp_title('|', true, 'right'); ?></title>
+		<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" >		
+		
+		<?php
+		// end around for wp_title changes https://make.wordpress.org/core/2014/10/29/title-tags-in-4-1/
+		if ( ! function_exists( '_wp_render_title_tag' ) ) :
+    		function theme_slug_render_title() {
+		?>
+		<title><?php wp_title( '|', true, 'right' ); ?></title>
+		<?php
+    	}
+    	add_action( 'wp_head', 'theme_slug_render_title' );
+		endif;
+		?>
+
 				
 		<?php if ( is_singular() ) wp_enqueue_script( "comment-reply" ); ?>
 		 
